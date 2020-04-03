fe420de9f43ffe06648e1edb73ddda2c7659bd83
nengak <nengakdakup@gmail.com>
2020-04-03 19:39:20 +0100

    pheew

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -12,13 +12,11 @@ import './App.css'
 class App extends Component{
   render(){
     return (
-      <BrowserRouter>
-        <Switch>
+      <HashRouter basename="/">
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-        </Switch>
-       </BrowserRouter> 
+       </HashRouter> 
     );
   }
 }
