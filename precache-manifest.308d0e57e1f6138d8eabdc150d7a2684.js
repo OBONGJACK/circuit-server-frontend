c06d029d00e79e0a5e9eb356a414c380a0aad5b3
nengak <nengakdakup@gmail.com>
2020-04-03 19:22:03 +0100

    Updates

---
+++ precache-manifest.308d0e57e1f6138d8eabdc150d7a2684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f4b8bc4ef0af60ae4fd0e778713387",
+    "url": "/circuit-server-frontend/index.html"
+  },
+  {
+    "revision": "a15b5350f3ce425ab559",
+    "url": "/circuit-server-frontend/static/css/main.8700b99a.chunk.css"
+  },
+  {
+    "revision": "8471977a98281dc57efd",
+    "url": "/circuit-server-frontend/static/js/2.12d73a9a.chunk.js"
+  },
+  {
+    "revision": "ebd2db610b93d62f27b7cdf4414b1820",
+    "url": "/circuit-server-frontend/static/js/2.12d73a9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15b5350f3ce425ab559",
+    "url": "/circuit-server-frontend/static/js/main.665aafe0.chunk.js"
+  },
+  {
+    "revision": "ab93f914f24cc318452c",
+    "url": "/circuit-server-frontend/static/js/runtime-main.085bb97d.js"
+  }
+]);
