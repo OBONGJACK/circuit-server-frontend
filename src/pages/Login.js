6f6f6d623846b265b92a5056a15747c29db96c3d
Nengak Dakup <nengakdakup@gmail.com>
2021-04-19 16:07:07 +0530

    minor changes

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 // Full NAme
 // Username
 // School ID
@@ -13,45 +13,65 @@ import Grid from '../assets/grid.png';
 import Renewable from '../assets/renewable.png';
 
 class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
 
         this.checkLogin = this.checkLogin.bind(this);
     }
 
-    checkLogin(){
+    checkLogin() {
         this.props.history.push('/');
     }
 
-    render(){
-        return (
-            <div className="login-wrap">
+    render() {
+        return ( <
+            div className = "login-wrap" >
 
-                <div className="login-left">
-                    <div className="login-content">
-                        <div className="images">
-                            <img src={Generator} alt="generator" />
-                            <img src={Grid} alt="Grid" />
-                            <img src={Renewable} alt="Renewable" />
-                        </div>
-                        <h1>Intelligent Master Controller Interface</h1>
-                        <div></div>
-                        <input type="mail" placeholder="Email" /><br />
-                        <input type="password" placeholder="Password" /><br />
-                        <button onClick={() => this.checkLogin()}>SIGN IN</button>
-                        <p><Link to="">Forgot Your Password?</Link></p>
-                    </div>
-                </div>
+            <
+            div className = "login-left" >
+            <
+            div className = "login-content" >
+            <
+            div className = "images" >
+            <
+            img src = { Generator }
+            alt = "generator" / >
+            <
+            img src = { Grid }
+            alt = "Grid" / >
+            <
+            img src = { Renewable }
+            alt = "Renewable" / >
+            <
+            /div> <
+            h1 > Intelligent Master Controller Interface < /h1> <
+            div > < /div> <
+            input type = "mail"
+            placeholder = "Email" / > < br / >
+            <
+            input type = "password"
+            placeholder = "Password" / > < br / >
+            <
+            button onClick = {
+                () => this.checkLogin()
+            } > SIGN IN < /button> <
+            p > < Link to = "" > Forgot Your Password ? < /Link></p >
+            <
+            /div> < /
+            div >
 
-                <div className="login-right">
-                    <div className="login-content">
-                        <h1>New Operator</h1>
-                        <p>Enter your personal details to sign up</p>
-                        <Link to="/signup">SIGN UP</Link>
-                        <p>POWER POOL SYSTEM</p>
-                    </div>
-                </div>
-            </div>
+            <
+            div className = "login-right" >
+            <
+            div className = "login-content" >
+            <
+            h1 > New Operator < /h1> <
+            p > Enter your personal details to sign up < /p> <
+            Link to = "/signup" > SIGN UP < /Link> <
+            p > AFIKPO UNIFIED ENERGY SYSTEM < /p> < /
+            div > <
+            /div> < /
+            div >
         )
     }
 }
--- src/views/Overview.js
+++ src/views/Overview.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import BulbStatus from '../components/BulbStatus';
 import DeviceStatus from '../components/DeviceStatus';
@@ -7,58 +7,88 @@ import { Device_Status } from '../components/Icons'
 import PviCard from '../components/pviCard';
 
 class Overview extends Component {
-    render(){
+    render() {
         const {
             activeSource,
             bulbStatus,
             deviceStatus,
             powerProduced,
             powerConsumed,
-            communities } = this.props.data;
-        
-            const {switchBulbStatus, switchDeviceStatus, setActiveSource, controlUser} = this.props;
+            communities
+        } = this.props.data;
 
-        return (
-            <div className="main-content">
-                <h1 className="app-title">
-                    Development of intelligent master controller for Hybridized power pool System Application 
-                </h1>
-                <div className="main-content-top">
-                    <PviCard title="GRID SOURCE" setActiveSource={setActiveSource} source="Grid" activeSource={activeSource} />
-                    <PviCard title="RENEWABLE SOURCE" setActiveSource={setActiveSource} source="Renewable" activeSource={activeSource} />
-                    <PviCard title="GENERATOR SOURCE" setActiveSource={setActiveSource} source="Generator" activeSource={activeSource} />
-                </div>
-                <div className="main-content-top">
-                    <PviCard title="GENERATED ENERGY" value={deviceStatus? powerProduced : '---'} />
-                    <PviCard title="CONSUMED ENERGY" value={bulbStatus && deviceStatus? powerConsumed : '---'} />
-                </div>
-                <div className="main-content-body">
-                    <DeviceStatus deviceStatus={deviceStatus} switchDeviceStatus={switchDeviceStatus} activeSource={activeSource} />
-                    <BulbStatus bulbStatus={bulbStatus} deviceStatus={deviceStatus} switchBulbStatus={switchBulbStatus} />
-                </div>
-                <div className="user-logs-wrap">
-                    <ul>
-                        {
-                        communities.map(name => {
-                            return (
-                                <li key={name.id}>
-                                    <div>
-                                        {name.name}
-                                    </div>
-                                    <div>
-                                        {name.status? Device_Status('on') : Device_Status('off')}
-                                    </div>
-                                    <div>
-                                        <button onClick={() => controlUser(name.id)}>{name.status? 'Turn Off' : 'Turn On'}</button>
-                                    </div>
-                                </li>
-                            )
-                        })
-                        }
-        
-                    </ul>
-                </div>
-            </div>
+        const { switchBulbStatus, switchDeviceStatus, setActiveSource, controlUser } = this.props;
+
+        return ( <
+            div className = "main-content" >
+            <
+            h1 className = "app-title" >
+            Development of intelligent master controller
+            for Hybridized power pool System Applications <
+            /h1> <
+            div className = "main-content-top" >
+            <
+            PviCard title = "GRID SOURCE"
+            setActiveSource = { setActiveSource }
+            source = "Grid"
+            activeSource = { activeSource }
+            /> <
+            PviCard title = "RENEWABLE SOURCE"
+            setActiveSource = { setActiveSource }
+            source = "Renewable"
+            activeSource = { activeSource }
+            /> <
+            PviCard title = "GENERATOR SOURCE"
+            setActiveSource = { setActiveSource }
+            source = "Generator"
+            activeSource = { activeSource }
+            /> <
+            /div> <
+            div className = "main-content-top" >
+            <
+            PviCard title = "GENERATED ENERGY"
+            value = { deviceStatus ? powerProduced : '---' }
+            /> <
+            PviCard title = "CONSUMED ENERGY"
+            value = { bulbStatus && deviceStatus ? powerConsumed : '---' }
+            /> <
+            /div> <
+            div className = "main-content-body" >
+            <
+            DeviceStatus deviceStatus = { deviceStatus }
+            switchDeviceStatus = { switchDeviceStatus }
+            activeSource = { activeSource }
+            /> <
+            BulbStatus bulbStatus = { bulbStatus }
+            deviceStatus = { deviceStatus }
+            switchBulbStatus = { switchBulbStatus }
+            /> <
+            /div> <
+            div className = "user-logs-wrap" >
+            <
+            ul > {
+                communities.map(name => {
+                    return ( <
+                        li key = { name.id } >
+                        <
+                        div > { name.name } <
+                        /div> <
+                        div > { name.status ? Device_Status('on') : Device_Status('off') } <
+                        /div> <
+                        div >
+                        <
+                        button onClick = {
+                            () => controlUser(name.id) } > { name.status ? 'Turn Off' : 'Turn On' } < /button> <
+                        /div> <
+                        /li>
+                    )
+                })
+            }
+
+            <
+            /ul> <
+            /div> <
+            /div>
         )
     }
 }
